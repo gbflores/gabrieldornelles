ec6bfe9dc5f489f7fc6efccce7368835415b9128
Guilherme <guilhermebflores@gmail.com>
2024-06-15 18:31:24 -0300

    Change age to 24 years.

--- src/app/components/experiences/experience.tsx
+++ src/app/components/experiences/experience.tsx
@@ -6,7 +6,7 @@ export function Experiences() {
     return (
         <div className="experiences">
             <SectionTitleProps text="Sobre" />
-            <p>Olá meu nome é Gabriel Dornelles, tenho 23 anos, estou trabalhando na área de T.I a mais de 7 anos, tenho histórico comprovado na atuação Suporte tecnico e externo, montagem e expansão de loja, manutenção de rede, Hardware e Softwares, de 2 grandes rede de farmacias no RS.</p>
+            <p>Olá meu nome é Gabriel Dornelles, tenho 24 anos, estou trabalhando na área de T.I a mais de 7 anos, tenho histórico comprovado na atuação Suporte tecnico e externo, montagem e expansão de loja, manutenção de rede, Hardware e Softwares, de 2 grandes rede de farmacias no RS.</p>
             <SectionTitleProps text="Conhecimentos" />
             <div className="knowledges">
                 <Knowledges />
